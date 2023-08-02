67587fa26f7244339ccadd242710017de942b07c
mig <michaelleojacob@gmail.com>
2023-08-01 18:47:16 -1000

    commit to share github. api routes returning 404

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,15 @@ import { useEffect } from "react";
 
 export default function Home() {
   const MakeFetch = async () => {
-    const rawRes = await fetch("/api/test");
-    console.log(rawRes);
-    // const res = await rawRes.json();
-    // console.log(res);
+    try {
+      const rawRes = await fetch("http://localhost:3000/pages/api/test");
+      console.log(rawRes);
+      rawRes.status ? console.log(rawRes.status) : null;
+      const res = await rawRes.json();
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   useEffect(() => {
@@ -16,6 +21,7 @@ export default function Home() {
   return (
     <>
       <div>hi</div>
+      <button onClick={MakeFetch}>refetch</button>
     </>
   );
 }
